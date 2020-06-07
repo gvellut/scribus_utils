1aed26e4946a6aa6d9e2739a33dd3da9691accfb
Guilhem Vellut <g@vellut.com>
2020-06-08 00:55:02 +0200

    Implemented import

--- Text_Import.py
+++ Text_Import.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import xml.etree.ElementTree as ET
 
 import scribus as sc
 
@@ -9,6 +10,8 @@ import scribus as sc
 
 KEY_PREFIX = "#---"
 COPY_PREFIX = "Copy of "
+NO_STYLE = "Default Paragraph Style"
+DEBUG = True
 
 # __file__ is not defined when running from Scribus but the dir of
 # the script being executed is in sys.path 0
@@ -66,17 +69,112 @@ def add_text(key, text, pstyles, cstyles):
     sc.selectText(0, 0, key)
     sc.deleteText(key)
 
-    sc.insertText(text, -1, key)
+    errors = []
+
+    root = ET.fromstring(text)
+    cursor_pos = 0
+    current_style = NO_STYLE
+    for p in root:
+        if p.text:
+            sc.insertText(p.text, -1, key)
+            cursor_pos += len(p.text)
+
+        for s in p:
+            if s.tag == "t":
+                if not s.text:
+                    errors.append("t tags should have text")
+                else:
+                    sc.insertText(s.text, -1, key)
+                    cursor_pos += len(s.text)
+
+                rt_l = cursor_pos - len(s.text)
+                sc.selectText(rt_l, -1, key)
+                if "f" in s.attrib:
+                    font_name = s.attrib["f"]
+                    try:
+                        sc.setFont(s.attrib["f"], key)
+                    except Exception:
+                        errors.append("Invalid font name: %s" % font_name)
+                if "c" in s.attrib:
+                    # not currently defined color name will be added with
+                    # a brown color
+                    # TODO but no way to tell ?
+                    sc.setTextColor(s.attrib["c"], key)
+                if "s" in s.attrib:
+                    font_size = s.attrib["s"]
+                    try:
+                        sc.setFontSize(int(font_size), key)
+                    except Exception:
+                        errors.append("Invalid font size: %s" % font_size)
+                sc.selectText(0, 0, key)  # deselect
+
+            elif s.tag == "cs":
+                if not s.text:
+                    errors.append("cs tags should have text")
+                else:
+                    sc.insertText(s.text, -1, key)
+                    cursor_pos += len(s.text)
+
+                rt_l = cursor_pos - len(s.text)
+                sc.selectText(rt_l, -1, key)
+                if "n" in s.attrib:
+                    char_style = s.attrib["n"]
+                    sc.setCharacterStyle(char_style, key)
+                else:
+                    errors.append("cs tags should have a 'n' attribute")
+                sc.selectText(0, 0, key)  # deselect
+
+            elif s.tag == "ps":
+                # change the state of the current style for this paragraph
+                # and the following
+                if "n" in p[0].attrib:
+                    current_style = p[0].attrib["n"]
+                    if not current_style:
+                        current_style = NO_STYLE
+                else:
+                    errors.append("ps tags should have a 'n' attribute")
+                if s.text:
+                    errors.append("ps tags should not have text")
+
+            # text as is
+            if s.tail:
+                sc.insertText(s.tail, -1, key)
+                cursor_pos += len(s.tail)
+
+        sc.insertText("\r", -1, key)
+        cursor_pos += 1
+        sc.selectText(cursor_pos - 1, -1, key)
+        sc.setStyle(current_style, key)
+        sc.selectText(0, 0, key)  # deselect
+
+    sc.deselectAll()
+    sc.docChanged(True)
+
+    if errors:
+        _error("Errors while processing '%s':\n- %s" % (key, "\n- ".join(errors)))
+
+
+def _debug(o):
+    if DEBUG:
+        sc.messageBox("DEBUG", repr(o))
+
+
+def _error(o, icon=sc.ICON_WARNING):
+    result = sc.messageBox(
+        "ERROR", o, icon, sc.BUTTON_OK | sc.BUTTON_DEFAULT, sc.BUTTON_ABORT,
+    )
+    if result == sc.BUTTON_ABORT:
+        sys.exit(1)
 
 
 def main(argv):
     if not sc.haveDoc():
-        sc.messageBox("Error", "Need a document")
-        return
+        _error("Need a document", sc.ICON_CRITICAL)
+        sys.exit(1)
 
     pstyles, cstyles = prep()
     texts = read_data(data_file)
-    sc.messageBox("Debug", repr(texts))
+    # _debug(texts)
 
     sc.setRedraw(False)
 
@@ -86,14 +184,10 @@ def main(argv):
         for i in range(num_selected):
             key = sc.getSelectedObject(i)
             if sc.getObjectType(key) != "TextFrame":
-                sc.messageBox(
-                    "Error", "Selected object with name %s not a text frame" % key
-                )
+                _error("Selected object with name %s not a text frame" % key)
                 continue
             if key not in texts:
-                sc.messageBox(
-                    "Error", "Selected text frame with name %s not in data" % key
-                )
+                _error("Selected text frame with name %s not in data" % key)
                 continue
     else:
         # go through the texts keys
@@ -106,19 +200,17 @@ def main(argv):
             if key.startswith("#"):
                 continue
 
-            sc.messageBox("DEBUG", key)
+            # sc.messageBox("DEBUG", key)
 
             # TODO keep around ? and display at the end ?
             if not sc.objectExists(key):
-                sc.messageBox("Error", "Object %s from data not found" % key)
+                _error("Object %s from data not found" % key)
                 continue
             if sc.getObjectType(key) != "TextFrame":
-                sc.messageBox("Error", "Object %s from data not a text frame" % key)
+                _error("Object %s from data not a text frame" % key)
                 continue
 
             add_text(key, texts[key], pstyles, cstyles)
-
-    sc.docChanged(True)
 
 
 def main_wrapper(argv):
@@ -138,53 +230,5 @@ if __name__ == "__main__":
     # texts = read_data(data_file)
     # print (repr(texts))
 
-"""
-obj = getSelectedObject(0)
-print (obj)
-selectText(0, 0, obj)
-deleteText(obj)
-
-style = "My Style"
-
-cursorPosition = 0
-print cursorPosition
-firstLineText = "First Line Text"
-
-insertText(firstLineText, -1, obj)
-cursorPosition = cursorPosition + len(firstLineText)
-insertText("\r", -1, obj)
-cursorPosition = cursorPosition + 1
-
-selectText(cursorPosition - 1, 1, obj)
-print (repr(getText(obj)))
-setStyle(style, obj)
-selectText(0, 0, obj)
-print ("unselect")
-
-secondLineText = "Second Line Text"
-lineStartPosition = cursorPosition
-insertText(secondLineText, -1, obj)
-cursorPosition = cursorPosition + len(secondLineText)
-insertText("\r", -1, obj)
-cursorPosition = cursorPosition + 1
-
-selectText(cursorPosition - 1, 1, obj)
-print (repr(getText(obj)))
-setStyle("Default Paragraph Style", obj)
-
-thirdLineText = "Third Line Text"
-insertText(thirdLineText, -1, obj)
-cursorPosition = cursorPosition + len(thirdLineText)
-insertText("\r", -1, obj)
-cursorPosition = cursorPosition + 1
-
-selectText(cursorPosition - 1, 1, obj)
-print (repr(getText(obj)))
-setStyle(style, obj)
-
-
-obj = getSelectedObject(0)
-print (obj)
-f = getFontFeatures()
-print (f)
-"""
+    # xml = '<text><paragraph><ps n="My Style" />Hello World.</paragraph><paragraph></paragraph><paragraph>Second line <t s="24">with</t> My Style</paragraph><paragraph><ps n="" />This line has no style <t f="Times New Roman" s="18" c="Red" >but has</t> char styles and <cs n="superscript">font</cs> change</paragraph></text>'
+    # root = ET.fromstring(xml)
